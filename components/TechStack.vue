8047a3021cda6633a3a58cef8e7b38ac12fd8208
Nikki <nikkiweat@gmail.com>
2024-01-17 10:07:34 -0600

    fix tech stack icons and github api request

--- components/TechStack.vue
+++ components/TechStack.vue
@@ -1,16 +1,18 @@
 <template>
   <div class="mt-16 md:mt-24">
-    <div class="flex justify-center items-center text-base font-semibold text-gray-600 dark:text-gray-300">
-      <h2 class="text-center">Core Tech Stack: </h2>
-      <IconDoubleDown class="h-4 w-4"/>
+    <div
+      class="flex justify-center items-center text-base font-semibold text-gray-600 dark:text-gray-300"
+    >
+      <h2 class="text-center">Core Tech Stack:</h2>
+      <IconDoubleDown class="h-4 w-4" />
     </div>
 
-
     <div class="flex flex-wrap justify-center items-center text-4xl mt-5">
-      <IconNode class="m-2 md:m-4"/>
-      <IconVuejs class="m-2 md:m-4"/>
-      <IconReact class="m-2 md:m-4"/>
-      <IconAngular class="m-2 md:m-4"/>
+      <IconNode class="m-2 md:m-4" />
+      <IconReact class="m-2 md:m-4" />
+      <IconAngular class="m-2 md:m-4" />
+      <IconVuejs class="m-2 md:m-4" />
+      <IconPython class="m-2 md:m-4" />
     </div>
   </div>
 </template>
